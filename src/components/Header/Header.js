0c785257fce76ff3b40bf286dfdb3106c01b1465
Nahidontheway~ <19203103053@cse.bubt.edu.bd>
2023-02-12 12:41:32 +0600

    authentication setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
 import './Header.css';
 
 const Header = () => {
+
+    const {user, logOut}= useContext(AuthContext);
+
     return (
         <nav className='header'>
             <h2>Medicine Shop</h2>
             <div>
                 <Link to="/shop">Shop</Link>
                 <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
+                <Link to="/inventory">Chekout</Link>
                 <Link to="/about">About</Link>
-                <Link to="/login">Login</Link> 
-                <Link to="/register">Register</Link> 
+                { user?.uid ?
+                    <button className='btn-logout' onClick={logOut}>Log out</button> :
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link> 
+                    </>
+                }
+                <span>{user?.email}</span>
             </div>
         </nav>
     );
